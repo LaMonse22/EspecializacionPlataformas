68b7aacfc99770e22f3668b5d0e7c27fef757914
LaMonse22 <marymonse.4@gmail.com>
2023-11-26 19:39:52 -0500

    Primer commit

---
+++ src/pedidos/Pedidos.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pedidos;
+
+import ui.PedidoForm;
+
+/**
+ *
+ * @author anl1.sui
+ */
+public class Pedidos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        PedidoForm pedidoForm = new PedidoForm();
+        pedidoForm.setVisible(true);
+        pedidoForm.setLocationRelativeTo(null);
+    }
+    
+}
