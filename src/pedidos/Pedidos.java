82cc8a3089e9f21d7ea3e485c70e8ed5f314c1ec
analista1.sui <analista1.sui@amigo.edu.co>
2023-11-29 07:44:26 -0500

    Ajustes finales

---
+++ src/pedidos/Pedidos.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pedidos;
+
+/**
+ *
+ * @author anl1.sui
+ */
+public class Pedidos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        PedidoForm pedidoForm = new PedidoForm();
+        pedidoForm.setVisible(true);
+        pedidoForm.setLocationRelativeTo(null);
+    }
+    
+}
